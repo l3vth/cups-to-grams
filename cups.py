66b802e4f5271868a4cfcf1b1ae8fab4a3ddd8b4
l3vth <132221005+l3vth@users.noreply.github.com>
2023-05-30 11:47:18 +0200

    Add files via upload

---
+++ cups.py
@@ -0,0 +1,22 @@
+print("")
+print("Cups to grams")
+
+input1 = input("How many cups?")
+
+cups = float(input1)
+
+grams1 = cups * 120
+
+grams = float(grams1)
+
+
+
+
+for x in [str(cups) + " cups are "+ str(grams) + "g ",]:
+    print(x)
+
+print("")
+print("")
+print("")
+print("")
+input("press any button to quit")
